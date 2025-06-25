cc4690652b3adc16989fc30f9430e8f6d18b31a8
Adrian Oliveira <adrianoliveir01@gmail.com>
2025-06-25 00:11:23 -0300

    Create ex1.py

---
+++ lista_01/ex1.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+#atividades propostas
+# 1 crie uam matriz 3x3 com valores aleatorios, calcule sua transposta, determinante e inversa
+
+mat1 = np.array([[5, 8, 4],
+                [6, 9, 5],
+                [4, 7, 2]])
+
+trnsmat1 = mat1.transpose()
+trnsmat1
+# 2 multiplique duas matrizes compatives e verifique a comutatividade
+# 3 Represente vetores de entrada e saida em um sistema de mistura com duas correntes de entrada
