23ca85d2f449c59ae76b02a8b2eb68330bae72e7
Adrian Oliveira <adrianoliveir01@gmail.com>
2025-06-25 13:36:25 -0300

    Realizei o exercício 2

--- lista_01/ex2_matrizes.py
+++ lista_01/ex2_matrizes.py
@@ -1,1 +1,22 @@
 # 2 multiplique duas matrizes compativeis e verifique a comutatividade
+import numpy as np
+
+A = np.array([[1,2,3],
+              [4,5,6]])
+
+B = np.array([[1,2],
+              [3,4],
+              [5,6]])
+
+print("\nMatriz A: \n", A, "\n\nMatriz B: \n", B)
+
+C = np.dot(A,B)
+D = np.dot(B,A)
+
+print("\nA multiplicação de AxB: \n", C)
+print("\nA multiplicação de BxA: \n", D)
+
+if np.array_equal(C,D):
+   print("\nA comutatividade se aplica as duas matrizes. (AxB = BXA)\n")
+else:
+   print("\nA comutatividade não se aplica as duas matrizes.(AxB != BXA)\n")
